0a6fc7bf6f26c1f392f1b13480e84cb69704ff47
Dana <danacastroman@gmail.com>
2026-02-12 20:42:14 -0300

    add redux and src folder (#2)

---
+++ src/app/postulaciones/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/src/components/app-shell"
+import { PostulacionesList } from "@/src/components/postulaciones-list"
+
+export default function PostulacionesPage() {
+  return (
+    <AppShell>
+      <PostulacionesList />
+    </AppShell>
+  )
+}
